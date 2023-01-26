400d198bb8a84b63dc0c413ae5715f5d8b5955d0
Peter Williams <peter@newton.cx>
2023-01-25 22:06:40 -0500

    Tidy up appearance of modals

--- src/index.rs
+++ src/index.rs
@@ -348,6 +348,16 @@ impl IndexCollection {
                 let loc = self.indices[iindex].get_location(entry.entry).unwrap();
                 let o = self.indices[OUTPUTS_INDEX_INDEX].resolve(loc.output);
                 let f = self.indices[FRAGMENTS_INDEX_INDEX].resolve(loc.fragment);
+
+                // I would like to strip off the explicit page name like this, but
+                // it seems to be confusing Parcel, and I don't feel like chasing the
+                // problem down right now.
+                //let o = if o.ends_with("/index.html") {
+                //    &o[..o.len() - 10]
+                //} else {
+                //    o
+                //};
+
                 writeln!(
                     tex,
                     r"\expandafter\def\csname pedia resolve**{}**{}**loc\endcsname{{{}{}}}",
--- web/ModalManager.vue
+++ web/ModalManager.vue
@@ -9,6 +9,18 @@
       'modal-wrapper': true,
       'modal-wrapper-visible': active != ModalKind.None,
     }">
+      <!-- This has the same layout as the app's menu bar, to provide a nice
+        title for the modal that matches the high-level page layout -->
+      <div id="modal-menu-bar" class="modal-menu-bar">
+        <h1 class="modal-menu-title" v-text="modalTitle"></h1>
+
+        <div class="right-buttons">
+          <button type="button" @click="clear" class="close-button" title="Close overlay" aria-label="Close overlay">
+            ×
+          </button>
+        </div>
+      </div>
+
       <!-- The "dispatch" modal is basically the "main menu" of the doc app that
         provides access to all of its functionality. We need to provide a way to
         get at everything without using a keyboard for mobile. -->
@@ -25,10 +37,6 @@
       <div v-show="active == ModalKind.Search" class="modal-container page-wrapper">
         <SearchModal ref="search"></SearchModal>
       </div>
-
-      <button type="button" @click="clear" class="close-button" title="Close overlay" aria-label="Close overlay">
-        ×
-      </button>
     </div>
   </div>
 </template>
@@ -86,12 +94,7 @@
 }
 
 .close-button {
-  position: fixed;
-  top: 0.5rem;
-  right: 0.5rem;
   font-size: 2em;
-  width: 3rem;
-  height: 3rem;
   border: none;
   border-radius: 5px;
 
@@ -103,22 +106,69 @@
     color: var(--icons-hover);
   }
 }
+
+// Not great -- duplicating the main app menu bar as specified in style.scss
+
+#modal-menu-bar {
+  margin: auto calc(0px - var(--page-padding));
+
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: var(--bg);
+  border-bottom-color: var(--bg);
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+
+  border-bottom-color: var(--table-border-color);
+
+  color: var(--icons);
+}
+
+.modal-menu-title {
+  display: inline-block;
+  font-weight: 200;
+  font-size: 1.3rem;
+  line-height: var(--menu-bar-height);
+  text-align: center;
+  margin: 0;
+  flex: 1;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.right-buttons {
+  margin: 0 15px;
+}
+
+.right-buttons a {
+  text-decoration: none;
+}
 </style>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import { ModalKind } from "./base";
 import DispatchModal from "./DispatchModal.vue";
 import HelpModal from "./HelpModal.vue";
 import SearchModal from "./SearchModal.vue";
 
 const active = ref(ModalKind.None);
-const search = ref()
+const search = ref();
+
+const modalTitle = computed(() => {
+  switch (active.value) {
+    case ModalKind.None: return "";
+    case ModalKind.Dispatch: return "Main Menu";
+    case ModalKind.Help: return "Help";
+    case ModalKind.Search: return "Search";
+  }
+});
 
 function clear() {
   active.value = ModalKind.None;
 }
-
 
 function toggleBasic(kind: ModalKind): boolean {
   if (active.value == kind) {
@@ -129,7 +179,6 @@ function toggleBasic(kind: ModalKind): boolean {
     return true;
   }
 }
-
 
 function toggleDispatch() {
   toggleBasic(ModalKind.Dispatch);
