37047222bfd37d370320fe5ba9fc571695eee6ec
Peter Williams <peter@newton.cx>
2023-02-06 21:48:02 -0500

    web/App.vue: get hidey menu bar to actually work

--- web/App.vue
+++ web/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <div id="page-wrapper" class="page-wrapper">
+    <div class="page-wrapper">
       <div class="page">
+        <!-- This element hangs out at the top of the window invisibly, so that we can
+          detect when the user hovers the mouse there and reveal the menu bar -->
         <div id="menu-bar-hover-placeholder"></div>
-        <div id="menu-bar" class="menu-bar sticky bordered">
+
+        <div ref="menuBar" id="menu-bar" class="menu-bar sticky bordered">
           <div class="left-buttons">
             <button class="icon-button" type="button" @click="onDispatchClicked">
               <FontAwesomeIcon icon="fa-solid fa-bars" />
@@ -42,6 +45,7 @@ const props = defineProps({
   relTop: { type: String, required: true },
 });
 
+const menuBar = ref();
 const modalManager = ref();
 
 // Local event handlers
@@ -95,14 +99,79 @@ function unmountKeybindings() {
   window.removeEventListener("keydown", onKeydown);
 }
 
+// Scroll monitoring for sticky menu bar
+//
+// This is all more-or-less copied out of mdBook.
+
+var onScroll = null;
+
+function mountScroll() {
+  var scrollTop = document.scrollingElement.scrollTop;
+  var prevScrollTop = scrollTop;
+  var minMenuY = -menuBar.value?.clientHeight - 50;
+
+  // When the script loads, the page can be at any scroll
+  menuBar.value!.style.top = scrollTop + 'px';
+  var topCache = menuBar.value?.style.top.slice(0, -2);
+
+  menuBar.value!.classList.remove('sticky');
+  var stickyCache = false;
+
+  onScroll = function onScroll(event: Event) {
+    const scrollTop = Math.max(document.scrollingElement.scrollTop, 0);
+
+    // `null` means that it doesn't need to be updated
+    var nextSticky = null;
+    var nextTop = null;
+    var scrollDown = scrollTop > prevScrollTop;
+    var menuPosAbsoluteY = topCache - scrollTop;
+
+    if (scrollDown) {
+      nextSticky = false;
+      if (menuPosAbsoluteY > 0) {
+        nextTop = prevScrollTop;
+      }
+    } else {
+      if (menuPosAbsoluteY > 0) {
+        nextSticky = true;
+      } else if (menuPosAbsoluteY < minMenuY) {
+        nextTop = prevScrollTop + minMenuY;
+      }
+    }
+
+    if (nextSticky === true && stickyCache === false) {
+      menuBar.value!.classList.add('sticky');
+      stickyCache = true;
+    } else if (nextSticky === false && stickyCache === true) {
+      menuBar.value!.classList.remove('sticky');
+      stickyCache = false;
+    }
+
+    if (nextTop !== null) {
+      menuBar.value!.style.top = nextTop + 'px';
+      topCache = nextTop;
+    }
+
+    prevScrollTop = scrollTop;
+  }
+
+  document.addEventListener("scroll", onScroll, { passive: true });
+}
+
+function unmountScroll() {
+  document.removeEventListener("scroll", onScroll);
+}
+
 // The hooks
 
 onMounted(() => {
   mountKeybindings();
+  mountScroll();
 });
 
 onUnmounted(() => {
   unmountKeybindings();
+  unmountScroll();
 });
 
 </script>
