7c415c5b99786bd796f711cb2a0e877caafa5f51
Peter Williams <peter@newton.cx>
2024-02-25 10:42:37 -0500

    serve-ui/src/YarnServeTab.vue: tidy a bit

--- serve-ui/src/YarnServeTab.vue
+++ serve-ui/src/YarnServeTab.vue
@@ -1,33 +1,48 @@
 <script setup lang="ts">
 // The tab with output from the `yarn serve` process.
-import { ref, type Ref } from "vue";
-import { NLog } from "naive-ui";
+import { ref } from "vue";
+import { NButton, NFlex } from "naive-ui";
 
 import type { ServerQuittingMessage, YarnOutputMessage } from "./messages.js";
 
-const lines: Ref<string[]> = ref([]);
+const log = ref("")
 
 function onYarnOutput(msg: YarnOutputMessage) {
-  lines.value.push(...msg.yarn_output.lines);
+  const s = msg.yarn_output.lines.join("\n") + "\n";
+  log.value += s;
 }
 
 function onServerQuitting(_msg: ServerQuittingMessage) {
   // The log component seems to elide empty lines, annoyingly.
-  lines.value.push(" ", "(server quitting)");
+  log.value += "\n(server quitting)";
+}
+
+function onClear() {
+  log.value = "";
 }
 
 defineExpose({ onServerQuitting, onYarnOutput });
 </script>
 
 <template>
-  <p>The output from <code>yarn serve</code>.</p>
+  <n-flex align="center" justify="end">
+    <p class="desc">The output from <code>yarn serve</code>.</p>
 
-  <n-log class="log" :lines="lines" />
+    <n-button @click="onClear" strong secondary type="error">Clear Output</n-button>
+  </n-flex>
+
+  <pre class="log"><code ref="code">{{ log }}</code></pre>
 </template>
 
 <style scoped>
+.desc {
+  flex: 1;
+}
+
 .log {
   width: 100%;
+  min-height: 10rem;
+  overflow: scroll;
   padding: 5px;
   color: #FFF;
   background-color: #000;
