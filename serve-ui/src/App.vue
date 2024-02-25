4418cb5894cb1ea4975ba8a502d83498b9140e9d
Peter Williams <peter@newton.cx>
2024-02-24 20:31:17 -0500

    serve-ui: start fleshing this out a bit more

--- serve-ui/src/App.vue
+++ serve-ui/src/App.vue
@@ -1,25 +1,51 @@
 <script setup lang="ts">
-import { NButton } from "naive-ui";
+import { NButton, NCard, NConfigProvider, NGlobalStyle, NTabPane, NTabs } from "naive-ui";
+import { parseMessage } from "./messages.js";
 
 const socket = new WebSocket(`ws://${window.location.host}/ws`);
 
-socket.addEventListener("open", (_event) => {
-  console.log("connected!");
-});
-
 socket.addEventListener("message", (event) => {
-  console.log("Message:", event.data);
+  try {
+    const structured = JSON.parse(event.data);
+    const msg = parseMessage(structured);
+    console.log("parsed OK:", msg);
+  } catch (e) {
+    console.warn(e);
+  }
 });
 
 function onQuit() {
   socket.send("quit");
 }
+
 </script>
 
 <template>
-  <p>Hello world.</p>
+  <n-config-provider>
+    <n-global-style />
+    <n-card title="Tectonopedia Build UI">
+      <template #header-extra>
+        <nav>
+          <n-button @click="onQuit">Quit</n-button>
+        </nav>
+      </template>
 
-  <n-button @click="onQuit">Quit</n-button>
+      <n-tabs type="card" size="large">
+        <n-tab-pane name="build" tab="Build">
+          <p>The build!</p>
+        </n-tab-pane>
+
+        <n-tab-pane name="yarn-serve" tab="yarn serve">
+          <p>The output from <code>yarn serve</code>.</p>
+        </n-tab-pane>
+      </n-tabs>
+    </n-card>
+  </n-config-provider>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  display: flex;
+  flex-direction: row;
+}
+</style>
---
+++ serve-ui/src/messages.ts
@@ -0,0 +1,84 @@
+// See also /src/messages.rs, which defines the message types on the Rust side.
+
+import * as S from "@effect/schema/Schema";
+import { formatError } from "@effect/schema/TreeFormatter";
+import * as Either from "effect/Either";
+
+const AlertMessage = S.struct({
+    message: S.string,
+    context: S.array(S.string),
+});
+
+const BuildCompleteMessage = S.struct({
+    build_complete: S.struct({
+        success: S.boolean,
+        elapsed: S.number,
+    }),
+});
+
+const BuildStartedMessage = S.literal("build_started");
+
+const CommandLaunchedMessage = S.struct({
+    command_launched: S.string,
+});
+
+const ErrorMessage = S.struct({
+    error: AlertMessage,
+});
+
+const NoteMessage = S.struct({
+    note: AlertMessage,
+});
+
+const PhaseStartedMessage = S.struct({
+    phase_started: S.string,
+});
+
+const ServerQuittingMessage = S.literal("server_quitting");
+
+const ToolOutputStreamStdout = S.literal("stdout");
+const ToolOutputStreamStderr = S.literal("stderr");
+
+const ToolOutputStream = S.union(
+    ToolOutputStreamStdout,
+    ToolOutputStreamStderr,
+);
+
+const WarningMessage = S.struct({
+    warning: AlertMessage,
+});
+
+const YarnOutputMessage = S.struct({
+    yarn_output: S.struct({
+        stream: ToolOutputStream,
+        lines: S.array(S.string),
+    }),
+});
+
+// S.attachPropertySignature might be helpful here but it causes TypeScript
+// errors for me.
+const Message = S.union(
+    BuildCompleteMessage,
+    BuildStartedMessage,
+    CommandLaunchedMessage,
+    ErrorMessage,
+    NoteMessage,
+    PhaseStartedMessage,
+    ServerQuittingMessage,
+    WarningMessage,
+    YarnOutputMessage,
+);
+
+export type Message = S.Schema.To<typeof Message>;
+
+const schemaParseMessage = S.decodeUnknownEither(Message);
+
+export function parseMessage(input: any): Message {
+    const result = schemaParseMessage(input);
+
+    if (Either.isRight(result)) {
+        return result.right;
+    }
+
+    throw new Error(`failed to parse input "${input}" as message: ${formatError(result.left)}`);
+}
